b91fd6c79f06b592a2d2c3f5b1c306e49fe4954a
Blai Fernández Bravo <94699337+blass53@users.noreply.github.com>
2022-03-24 01:11:46 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import { useState } from 'react';
+import List from './components/List';
+import Form from './components/Form';
+
+// nuevos enlaces
+function App() {
+  const [list, setList] = useState(null);
+  const api = async() => {
+    const api = await fetch('http://localhost:8080/api/tutorials');
+    const data = await api.json();
+    setList(data);
+  }
+
+  const [semaforo, setSemaforo] = useState(false);
+  const published = async() => {  
+    if(semaforo){
+      api();
+      setSemaforo(false);
+      } else {
+      const api = await fetch('http://localhost:8080/api/tutorials/published');
+      const data = await api.json();
+      setList(data);
+      setSemaforo(true);
+    }
+    console.log(semaforo);
+  }
+
+  function home(){
+    setList(null);
+  }
+
+  return (
+    <div className="App">
+      <header className="header">
+        <nav>
+          <ul>
+            <li onClick={home}>Home</li>
+            <li onClick={api}>List</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        {list ? (<>
+          <span className='publicados' onClick={published}>{semaforo ? (<>TODOS</>) : (<>PUBLICADOS</>)}</span>
+            <List list={list} />
+          </>) :(
+          <>
+            <Form />
+          </>
+        )} 
+      </main>
+    </div>
+  );
+}
+
+export default App;
